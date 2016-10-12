26bf9c9acda92526fd3ecc70a8f3de61f7008906
Karol Poplawski <Karol Poplawski>
2016-10-12 18:06:09 +0200

    christmas tree playground

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function rysujChoinke(x){
+ 	var star = [];
+	for(i = 1; i <= x; i ++){
+    star = [];
+		for(j = 1; j <= i*2-1; j ++){
+		star.push("*");
+		}
+		console.log(star.join(''));
+	} 
+}
